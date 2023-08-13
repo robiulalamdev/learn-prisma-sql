c45e8815b4b4c1bd02f6e6384eb9ea033d0e1ef5
robiulalam76 <robiulalam762400@gmail.com>
2023-08-13 22:52:32 +0600

    server is ready

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express,{Application} from "express"
+import cors from "cors"
+const app :Application = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+export default app;
